d623ea48b8d0eb442c36b4a9bdb35cfb8ee84f7c
Long <nguye3lg@mail.uc.edu>
2022-09-18 22:30:05 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double springCost=500;
+        double summerCost=700;
+        double fallCost=1500;
+        double winterCost=9000;
+        double total=0;
+
+        total = springCost + summerCost + fallCost + winterCost;
+        System.out.println("The home maintance cost in spring: " + springCost);
+        System.out.println("The home maintance cost in summer: " + summerCost);
+        System.out.println("The home maintance cost in fall: " + fallCost);
+        System.out.println("The home maintance cost in winter: " + winterCost);
+        System.out.println("The total home maintance cost in spring: " + total);
+
+    }
+}
